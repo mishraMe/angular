45a6abb47689b7e6248318e98ca903b7870e0b14
Meenakshi Mishra <minakshi.mishra.us@gmail.com>
2020-11-23 17:52:00 -0500

    adding folders to picture selector

--- madd/src/app/admin-dashboard/picture-selector/picture-selector.component.ts
+++ madd/src/app/admin-dashboard/picture-selector/picture-selector.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject, onDestroy} from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { Component, OnInit, Inject, OnDestroy} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-picture-selector',
   templateUrl: './picture-selector.component.html',
@@ -9,12 +9,18 @@ export class PictureSelectorComponent implements OnInit {
 
   action: String;
   private sub: any;
+  srcFolder : String;
+  destFolder : String;
+  allPictures :  any;
+
   constructor( private route : ActivatedRoute) { }
 
   ngOnInit(): void {
     this.sub = this.route.params.subscribe(params =>{
       this.action = params['action'];
     });
+    this.srcFolder = '/Users/mishr/OneDrive/Pictures/' + this.action + 'Pictures';
+    this.destFolder = 'src/assets/' + this.action + '-images';
   }
 
   ngOnDestroy(): void {
